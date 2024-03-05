f12814fc1e307e1458b29433d0a58a1568eb3f34
ittoogami <thiagoramosassuncao@gmail.com>
2024-03-04 21:52:19 -0300

    criacao de um novo projeto

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,24 @@
+public class Operadores {
+    public static void main(String[] args) {
+        // String nomeComplete = "LINGUAGEM "+"JAVA";
+        // System.out.println(nomeComplete);
+
+        // String concatenacao = "?";
+        // System.out.println(concatenacao);
+        // concatenacao = 1+1+1+"1";
+        // System.out.println(concatenacao);
+        // concatenacao = 1+"1"+1+1;
+        // System.out.println(concatenacao);
+        // concatenacao = 1+"1"+1+"1";
+        // System.out.println(concatenacao);
+        // concatenacao = "1"+1+1+1;
+        // System.out.println(concatenacao);
+        // concatenacao = "1"+(1+1+1);
+        // System.out.println(concatenacao);
+
+        int numero = 5;
+
+        System.out.println(++numero);
+
+    }
+}
