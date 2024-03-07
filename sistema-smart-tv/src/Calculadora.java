f7f539c301193607ab770a497ad1e17b30819e50
ittoogami <thiagoramosassuncao@gmail.com>
2024-03-07 20:58:56 -0300

    criacao da class Calculadora

---
+++ sistema-smart-tv/src/Calculadora.java
@@ -0,0 +1,27 @@
+/**
+ *  <h1>Calculadora</h1>
+ *  A Calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Note:</b> Leia antentamente a documentação desta classe para
+ * desfrutar dos recursos oferecidos pelo autor
+ * 
+ * 
+ * @author Thiago Ramos
+ * @version 1.0
+ * @since   07/03/2024
+ * 
+ */
+
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números
+     */
+
+
+     public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+     }
+}
